54ff69866c31da058eb389508e5667c649930ced
Miguel Gobbo <miguelgobbobernardi@gmail.com>
2026-05-13 15:04:47 -0300

    Add files via upload

---
+++ core.ts
@@ -0,0 +1,26 @@
+class Item {
+ constructor (public title: string){
+
+ }
+}
+
+class TodoList {
+private items: Item[] = [];
+private filePath: String;
+
+constructor(filePath: string) {
+this.filePath = filePath
+}
+
+addItem(item: Item) {
+this.items.push(item)
+}
+
+removeItem(index: number){
+this.items.splice(index,1);
+}
+
+geiItems(): Item [] {
+    return this.items;
+}
+}
